fad7025fad1585966684a2e12693f85065c90b1e
pankajmasaye88 <pankajmasaye88.com>
2022-03-30 11:57:01 +0530

    angular basic

---
+++ Phase 4/Session work/Session 4/first.js
@@ -0,0 +1,8 @@
+var num1;
+num1 = 23;
+var fname = "Pankaj";
+var lname = "Masaye";
+console.log("Number: " + num1);
+console.log("fname: " + fname);
+console.log("fname: " + lname);
+console.log(fname + " " + lname);
---
+++ Phase 4/Session work/Session 4/first.ts
@@ -0,0 +1,10 @@
+let num1:number;
+num1=23;
+
+let fname:string="Pankaj";
+let lname:string="Masaye";
+
+console.log("Number: "+num1);
+console.log("fname: "+fname);
+console.log("fname: "+lname);
+console.log(fname+" "+lname);
